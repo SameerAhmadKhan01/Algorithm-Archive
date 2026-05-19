50dfe5f6df4b766597febdd7777812ae668b8b4c
Sameer Ahmad Khan <sameerahmadk06@gmail.com>
2026-05-19 22:06:01 +0530

    Added Recursive functions for post order and inorder tree traversal

--- 9. Trees/BinaryTree.c
+++ 9. Trees/BinaryTree.c
@@ -1,61 +1,83 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include "Queue.h"
 
 struct Node *root = NULL;
 
-void Treecreate(){
-    struct Node *p , *t;
+void Treecreate()
+{
+    struct Node *p, *t;
     int x;
     struct Queue q;
-    create(&q,100);
+    create(&q, 100);
 
     printf("Enter root value : ");
-    scanf("%d",&x);
+    scanf("%d", &x);
     root = (struct Node *)malloc(sizeof(struct Node));
     root->data = x;
-    root->lchild=root->rchild=NULL;
-    enqueue(&q,root);
+    root->lchild = root->rchild = NULL;
+    enqueue(&q, root);
 
-    while(!isEmpty(q)){
-        p=dequeue(&q);
-        printf("Enter Left Child of %d : ",p->data);
-        scanf("%d",&x);
-        if(x!=-1){
-            t=(struct Node *)malloc(sizeof(struct Node));
+    while (!isEmpty(q))
+    {
+        p = dequeue(&q);
+        printf("Enter Left Child of %d : ", p->data);
+        scanf("%d", &x);
+        if (x != -1)
+        {
+            t = (struct Node *)malloc(sizeof(struct Node));
             t->data = x;
             t->lchild = t->rchild = NULL;
             p->lchild = t;
-            enqueue(&q,t);
+            enqueue(&q, t);
         }
-         printf("Enter Right Child of %d : ",p->data);
-        scanf("%d",&x);
-        if(x!=-1){
-            t=(struct Node *)malloc(sizeof(struct Node));
+        printf("Enter Right Child of %d : ", p->data);
+        scanf("%d", &x);
+        if (x != -1)
+        {
+            t = (struct Node *)malloc(sizeof(struct Node));
             t->data = x;
             t->lchild = t->rchild = NULL;
             p->rchild = t;
-            enqueue(&q,t);
+            enqueue(&q, t);
         }
     }
 }
 
-
-void preorder(struct Node *p){
-    if(p){
-        printf("%d\t",p->data);
+void preorder(struct Node *p)
+{
+    if (p)
+    {
+        printf("%d\t", p->data);
         preorder(p->lchild);
         preorder(p->rchild);
     }
 }
 
+void Inorder(struct Node *p)
+{
+    if (p)
+    {
+        Inorder(p->lchild);
+        printf("%d ", p->data);
+        Inorder(p->rchild);
+    }
+}
+void Postorder(struct Node *p)
+{
+    if (p)
+    {
+        Postorder(p->lchild);
+        Postorder(p->rchild);
+        printf("%d ", p->data);
+    }
+}
 
-
-int main(){
+int main()
+{
     Treecreate();
-    
+
     preorder(root);
 
     return 0;
-
 }
