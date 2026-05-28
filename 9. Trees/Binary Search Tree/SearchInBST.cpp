455fe7751dc6b4064da86a4b13aa61b170c62830
Sameer Ahmad Khan <sameerahmadk06@gmail.com>
2026-05-28 21:58:51 +0530

    Recursive Implementation of Searching in a BST

---
+++ 9. Trees/Binary Search Tree/SearchInBST.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+// Node structure
+struct Node
+{
+    Node *left;
+    int data;
+    Node *right;
+    Node() : data(0), left(nullptr), right(nullptr) {}
+    Node(int data) : data(this->data), left(nullptr), right(nullptr) {}
+};
+
+// Recursive Function to search in a binary tree
+Node *Rsearch(Node *t, int key)
+{
+    if (t == nullptr)
+        return nullptr;
+    else if (t->data == key)
+        return t;
+    else if (key > t->data)
+        return Rsearch(t->right, key);
+    else
+        return Rsearch(t->left, key);
+}
+
+
